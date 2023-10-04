57f0522cd3ee36ebcdc7ee25e962f0dcb5eb39ef
Will Cooley <william.cooley@gmail.coom>
2023-10-04 12:18:06 -0400

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,12 @@
 // Code Keypad Component Here
 
+import React from "react";
+
 function Keypad (){
-    return (
-        <div></div>
-    )
+    function renderInput(){
+        console.log('Entering password...')
+    }
+    return <input type="password" onChange={renderInput}></input>
 }
 
 export default Keypad;
